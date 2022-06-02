de86022025178deef21f349d70e0365f62b8a52a
Flavio J Tenenbojm <103946021+Flavio-T-BR@users.noreply.github.com>
2022-06-02 18:57:41 -0300

    Add files via upload

---
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -0,0 +1,31 @@
+package me.dio.academia.digital.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "tb_matriculas")
+public class Matricula {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
+
+  private LocalDateTime dataDaMatricula = LocalDateTime.now();
+
+    public ChronoLocalDateTime<LocalDate> get() {
+    return getDataDaMatricula();}
+}
